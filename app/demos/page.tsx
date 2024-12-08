16a4b61f1d2bcc0b1b2a2f8251a6e4223ae25b02
princehabeeb <habiy3400@gmail.com>
2024-12-08 22:08:19 +0100

    update page.tsx

--- app/demos/page.tsx
+++ app/demos/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from "@/components/home/Navbar";
-import PrebuiltPages from "@/components/home/PrebuiltPages";
 
 const DemoPage = () => {
   return (
     <main>
       <Navbar />
-      <PrebuiltPages />
     </main>
   );
 };
